177d9a112b2044c19d0200ad5c17dee450a547e6
Kirby Hutton <kirby.hutton@gmail.com>
2019-02-11 23:29:00 +0000

    got homepage route working, starting on registration route

--- server.py
+++ server.py
@@ -17,3 +17,67 @@ app.secret_key = "ABC"
 # silently. This is horrible. Fix this so that, instead, it raises an
 # error.
 app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def index():
+    """Homepage."""
+    return render_template("homepage.html")
+
+
+@app.route("/register")
+def get_user_login_info():
+    """Show  user registration  page ."""
+    
+    return render_template("registration.html")
+
+
+@app.route("/user_login")
+def login_user():
+    """Allow existing user to login ."""
+    
+    return render_template("user_login.html")
+
+@app.route("/user_login", methods = ["POST"])
+def verify_user_login():
+    """make sure that password is correct."""
+
+    user_email = request.form.get('user_email')
+    password = request.form.get('password')
+
+
+    user = User.query.filter_by(email = user_email).first()
+    print(user)
+
+    if user == None:
+        flash("Registration not found, redirecting to registration.")
+        return redirect('/register')
+
+    elif user.password == password:
+        
+
+        session["user_id"] = user.user_id
+        print(session["user_id"])
+        flash("Logged in!")
+        return redirect('/')
+
+    else:
+        flash("Error, please try logging in again.")
+        return redirect('/user_login')
+
+
+
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    # make sure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
