228547e83ba778be36122fe0ef68990abef0b0c0
Kirby Hutton <kirby.hutton@gmail.com>
2019-02-21 22:14:11 +0000

    getting popups to work on test_map_3

--- server.py
+++ server.py
@@ -418,7 +418,19 @@ def test_map_functionality():
 
     user_list = User.query.all()
 
-    return render_template("test_map.html", user_list = user_list)  
+    return render_template("test_map.html", user_list = user_list) 
+
+@app.route("/test_map_2")
+def test_map_with_popups():
+    """Route purely for testing OSM maps with popup functionality"""
+
+    return render_template("test_map_2.html") 
+
+@app.route("/test_map_3")
+def test_map_with_functioning_popups():
+    """Route purely for testing OSM maps with popup functionality"""
+
+    return render_template("test_map_3.html")
 
 
 
